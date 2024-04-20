54a6533127720d8ea2db4b796a69ab156afd9e1d
Isuru Fernando <isuruf@gmail.com>
2024-04-20 11:47:35 -0500

    remove redundancy

--- recipe/install_binutils_symlinks.sh
+++ recipe/install_binutils_symlinks.sh
@@ -3,7 +3,7 @@ set -x
 
 CHOST="${triplet}"
 
-TOOLS="addr2line ar as c++filt dwp elfedit gprof ld ld.bfd ld.gold nm objcopy objdump ranlib readelf size strings strip"
+TOOLS="addr2line ar as c++filt elfedit gprof ld ld.bfd nm objcopy objdump ranlib readelf size strings strip"
 
 if [[ "$cross_target_platform" == "linux-"* ]]; then
   TOOLS="${TOOLS} dwp ld.gold"
