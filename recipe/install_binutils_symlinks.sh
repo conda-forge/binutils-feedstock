0ebb0927a27f89306b50e03f54905fe41d8fdff4
Isuru Fernando <isuruf@gmail.com>
2024-04-20 11:27:55 -0500

    more typos

--- recipe/install_binutils_symlinks.sh
+++ recipe/install_binutils_symlinks.sh
@@ -5,7 +5,7 @@ CHOST="${triplet}"
 
 TOOLS="addr2line ar as c++filt dwp elfedit gprof ld ld.bfd ld.gold nm objcopy objdump ranlib readelf size strings strip"
 
-if [[ "cross_target_platform" == "linux-"* ]]; then
+if [[ "$cross_target_platform" == "linux-"* ]]; then
   TOOLS="${TOOLS} dwp ld.gold"
   ln -s "$PREFIX/bin/ld.gold" "$PREFIX/bin/gold"
 fi
